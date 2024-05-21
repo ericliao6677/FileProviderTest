42dd516de706b0d7f801cb4e2e33fdfc2339d23a
ericliao <chixiang777@gmail.com>
2024-05-21 08:38:29 +0800

    加入專案檔案。

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Drawing;
+using System.Security.Cryptography;
+
+namespace FileProviderTest.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        
+        private readonly IFileProvider _fileProvider;        
+
+        public FileUploadController(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;          
+        }
+
+        [HttpGet]
+        public IActionResult GetImage([FromQuery] string fileName)
+        {           
+            //var fileInfo = _fileProvider.GetFileInfo($"/{fileName}");
+            //return File(fileInfo.CreateReadStream(), "image/jpeg");
+
+            var fileInfo = _fileProvider.GetFileInfo(fileName);
+            if (!fileInfo.Exists)
+            {
+                return NotFound();
+            }
+
+            // 讀取文件內容並返回
+            using (var stream = fileInfo.CreateReadStream())
+            {
+                var memoryStream = new MemoryStream();
+                stream.CopyTo(memoryStream);
+                memoryStream.Position = 0;
+                return File(memoryStream, "application/octet-stream", fileName);
+            }
+        }
+    
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            var fileProvider = _fileProvider as PhysicalFileProvider;
+            var filePath = Path.Combine(fileProvider.Root, Path.GetRandomFileName() + ".jpg");
+           
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return Ok("上傳成功");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+var path = Directory.GetCurrentDirectory();
+var path2 = builder.Environment.ContentRootPath;
+var path3 = Path.GetTempFileName();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")));
+
+
+builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(@"D:\FileTest"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
